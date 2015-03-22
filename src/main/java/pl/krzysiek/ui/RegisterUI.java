1134a8b6ab164115e10e14f231a42eb83326b26d
Krzysztof Lamkiewicz <krzysztof.lamkiewicz@gmail.com>
2015-03-22 21:02:27 +0100

    Registration servlet fix

--- src/main/java/pl/krzysiek/ui/RegisterUI.java
+++ src/main/java/pl/krzysiek/ui/RegisterUI.java
@@ -1,10 +1,14 @@
 package pl.krzysiek.ui;
 
+import javax.servlet.annotation.WebServlet;
+
 import pl.krzysiek.model.User;
 import pl.krzysiek.view.LoginView;
 import pl.krzysiek.view.RegisterView;
 
+import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
 import com.vaadin.server.VaadinSession;
 import com.vaadin.ui.UI;
 
@@ -30,4 +34,11 @@ public class RegisterUI extends UI {
 			setContent(registerView);
 		}
 	}
+	
+	@WebServlet(value = "/register/*", asyncSupported = true)
+	@VaadinServletConfiguration(productionMode = false, ui = RegisterUI.class)
+	public static class Servlet extends VaadinServlet {
+
+	}
+
 }
--- src/main/java/pl/krzysiek/view/RegisterView.java
+++ src/main/java/pl/krzysiek/view/RegisterView.java
@@ -5,13 +5,47 @@ import pl.krzysiek.data.DataContainer;
 
 import com.vaadin.data.Validator;
 import com.vaadin.data.Validator.InvalidValueException;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.PasswordField;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Button.ClickEvent;
 
 public class RegisterView extends VerticalLayout {
 
+	public RegisterView(){
+		addComponent(new FormLayoutClass());
+	}
 	private class FormLayoutClass extends CustomComponent {
+		private VerticalLayout v;
+		private Button register;
+		private TextField username;
+		private PasswordField password;
+		private PasswordField matchingPassword;
+		
+		public FormLayoutClass(){
+			v = new VerticalLayout();
+			setCompositionRoot(v);
+			register = new Button("Register");
+			username = new TextField("Username: ");
+			username.addValidator(new UsernameValidator());
+			password = new PasswordField("Password: ");
+			matchingPassword = new PasswordField("Matching password: ");
+			
+			v.addComponent(username);
+			v.addComponent(password);
+			v.addComponent(matchingPassword);
+			v.addComponent(register);
 
+			register.addClickListener(new Button.ClickListener() {
+				
+				@Override
+				public void buttonClick(ClickEvent event) {
+					// TODO
+				}
+			});
+		}
 	}
 
 	private class UsernameValidator implements Validator {
@@ -27,6 +61,15 @@ public class RegisterView extends VerticalLayout {
 				throw new InvalidValueException("User already exists");
 			}
 		}
+	}
+	
+	private class PasswordValidator implements Validator{
 
+		@Override
+		public void validate(Object value) throws InvalidValueException {
+			// TODO 
+			
+		}
+		
 	}
 }
